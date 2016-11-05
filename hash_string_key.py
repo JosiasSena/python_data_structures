e7fcc378feaef84e41c5b350d7639a6bb89bf0c6
Josias Sena <josias.sena176@gmail.com>
2016-11-04 20:24:30 -0400

    remove

--- hash_string_key.py
+++ hash_string_key.py
@@ -1,33 +1,19 @@
-"""Write a HashTable class that stores strings
-in a hash table, where keys are calculated
-using the first two letters of the string."""
-
 
 class HashTable(object):
     def __init__(self):
         self.table = [None] * 10000
 
     def store(self, string):
-        """Input a string that's stored in
-        the table."""
-
         self.table.insert(self.calculate_hash_value(string), string)
         pass
 
     def lookup(self, string):
-        """Return the hash value if the
-        string is already in the table.
-        Return -1 otherwise."""
-
         if self.table.__contains__(string):
             return self.calculate_hash_value(string)
         else:
             return -1
 
     def calculate_hash_value(self, string):
-        """Helper function to calulate a
-        hash value from a string."""
-
         return (ord(string[0]) * 100) + ord(string[1])
 
 
